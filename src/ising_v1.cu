29eb5ae0dce1d0a669ec5e7694324c0e7c9c04ae
Alexandros Tsiggilis <alextsin@aristotle1.it.auth.gr>
2020-01-05 17:14:42 +0200

    v1

--- src/ising_v1.cu
+++ src/ising_v1.cu
@@ -12,26 +12,40 @@
 #define			weight(i,j)			w[ (2+i)*5 + (2+j) ]
 
 
-__global__ void kernel( int *X, int* Y, double *w, int n) {
+__global__ void kernel( int *X, int* Y, double *w, int k, int n) {
 
 	int row_index = blockIdx.x * blockDim.x + threadIdx.x,
 			coll_index = blockIdx.y * blockDim.y + threadIdx.y,
 			row_stride = gridDim.x * blockDim.x,
 			coll_stride = gridDim.y * blockDim.y;
 
+	while( k > 0) {
 
-			for( int i = row_index; i < n; i += row_stride ) {
-				for( int j = coll_index; j < n; j += coll_stride) {
+		if(row_index == 0 && coll_index == 0) {
+			int *tmp = X;
+			X = Y;
+			Y = tmp;
+		}
 
-					double ws = 0;
-					for(int l = -2; l <= 2; l++)
-						for(int m = -2; m <= 2; m++)
-							ws += weight(l,m) * Xmat(i+l, j+m);
+		for( int i = row_index; i < n; i += row_stride ) {
+			for( int j = coll_index; j < n; j += coll_stride) {
 
-					Ymat(i,j) = update(Xmat(i,j), ws);
+				double ws = 0;
+				for(int l = -2; l <= 2; l++)
+					for(int m = -2; m <= 2; m++)
+						ws += weight(l,m) * Xmat(i+l, j+m);
 
-				}
+				// Update Y	
+				const double acc=1.0e-8;
+				int sgn =  (( ws > acc) - (ws < -acc));
+				Ymat(i,j) = (Xmat(i,j)*(sgn == 0) + sgn*(sgn!=0));
 			}
+		}
+	
+		k--;
+		__syncthreads();
+	}
+
 }
 
 void ising(int* G, double* w, int k, int n) {
@@ -46,18 +60,14 @@ void ising(int* G, double* w, int k, int n) {
 	cudaMemcpy(Y,G, n*n, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_w, w, 5*5, cudaMemcpyHostToDevice);
 
-	while( k > 0) {
 
-		swap_mat( &X, &Y );
 	
-		dim3 N( MAX_BLOCKS, MAX_BLOCKS ),
-				 M( MAX_THREADS, MAX_THREADS );
+	dim3 N( MAX_BLOCKS, MAX_BLOCKS ),
+			 M( MAX_THREADS, MAX_THREADS );
 	
-		kernel<<<N,M>>>(X,Y,w,n);
+	kernel<<<N,M>>>(X,Y,w,k,n);
 
-		k--;
-	}
-
+	
 	cudaMemcpy(G, Y, n*n, cudaMemcpyDeviceToHost);
 
 	cudaFree(X); cudaFree(Y), cudaFree(d_w);
@@ -66,27 +76,3 @@ void ising(int* G, double* w, int k, int n) {
 
 }
 
-
-__device__ static inline int update(int old_value, double ws) {
-
-	int sign = sgn(ws);
-
-	return (old_value*(sign == 0) + sign*(sign!=0));
-
-}
-
-static void swap_mat(int **G, int **H) {
-
-	int *tmp = *H;
-	*H = *G;
-	*G = tmp;
-
-
-}
-
-static inline int sgn (double d){
-
-	const double acc=1.0e-8;
-
-	return (( d > acc) - (d < -acc));
-}
